d6b64d1138e9222677ca1d0fc392efea72128219
Chapito <hasan.ayvat4@gmail.com>
2022-04-15 17:07:01 +0300

    a

--- resources/views/kayit.blade.php
+++ resources/views/kayit.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-  	<title>Giriş Yap</title>
+  	<title>Login 10</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -12,7 +12,7 @@
 	<link rel="stylesheet" href="css/style.css">
 
 	</head>
-	<body class="img js-fullheight" style="background-image: url(images/bg.jpg);">
+	<body class="img js-fullheight" style="background-image: url(images/bg2.jpg);">
 	<section class="ftco-section">
 		<div class="container">
 			<div class="row justify-content-center">
@@ -20,7 +20,7 @@
 
 				</div>
 			</div>
-			<div class="row ">
+			<div class="row justify-content-right ">
 				<div class="col-md-6 col-lg-4">
 					<div class="login-wrap p-0">
 		      	<h3 class="mb-4 text-center">Giris Yap</h3>
@@ -36,17 +36,13 @@
 	            	<button type="submit" class="form-control btn btn-primary submit px-3">Giriş Yap</button>
 	            </div>
 	            <div class="form-group d-md-flex">
-	            	<div class="w-50">
-		            	<label class="checkbox-wrap checkbox-primary">Beni Hatırla
-									  <input type="checkbox" checked>
-									  <span class="checkmark"></span>
-									</label>
+		            	<div class="w-50 text-md-left">
+									<a href="#" style="color: #fff">Kayıt ol</a>
 								</div>
+
 								<div class="w-50 text-md-right">
 									<a href="#" style="color: #fff">Şifremi Unuttum ?</a>
 								</div>
-
-
 	            </div>
 	          </form>
 		      </div>
